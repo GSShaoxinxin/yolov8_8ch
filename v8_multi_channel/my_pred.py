3acd8908d0c3429cdcd7f907bf52da732c00dff5
GSShaoxinxin <3264404019@qq.com>
2025-04-29 19:07:15 +0800

    Update my_pred.py

--- v8_multi_channel/my_pred.py
+++ v8_multi_channel/my_pred.py
@@ -6,8 +6,8 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 ###########yolov8多通道推理##########
 if __name__ == '__main__':
-    model = YOLO(r"D:\Shao\Files\appdata\PycharmProject\v8\v8_ch\v8_multi_channel\runs\detect\train20\weights\best.pt")
-    path = r"D:\Shao\Files\appdata\PycharmProject\v8\v8_ch\datasets_my_multi\test.txt"
+    model = YOLO(r"D:\yolov8_8ch\v8_multi_channel\runs\detect\train20\weights\best.pt")
+    path = r"D:\yolov8_8ch\datasets_my_multi\test.txt"
     model.predict(source=path, ch=8, save=True, conf=0.5, save_txt=True, save_conf=True)
 
 
